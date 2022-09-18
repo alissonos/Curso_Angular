0abea64337e022a884a1f5514c5d8ee544cdbf4c
alissonos <alissonoliveirasilva.oliveira@gmail.com>
2022-09-17 21:57:56 -0300

    Compartilhamento de Dados via @input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Artur';
+
+  userData = {
+    email: 'artur@gmail.com',
+    role: 'Admin',
+  };
+
   title = 'Curso_angular';
 }
